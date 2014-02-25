ab4afa37f18a5b2e3c715ab650944b2f2b829ddf
Tech Support <admin2@IT-3.local>
2014-02-24 21:34:34 -0500

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Tech Support on 2/24/14.
+//  Copyright (c) 2014 iSonny Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Tech Support on 2/24/14.
+//  Copyright (c) 2014 iSonny Inc. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
